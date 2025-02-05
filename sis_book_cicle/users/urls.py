eab07b047e60557a0ffd5fb6fb12a3193864594a
Meyklaw <maximnoch@gmail.com>
2025-02-05 09:56:42 +0300

    requir

--- sis_book_cicle/users/urls.py
+++ sis_book_cicle/users/urls.py
@@ -1,6 +1,4 @@
-
 from django.urls import path
-from django.contrib.auth import views as reset
 from . import views as sigma
 from django.contrib.auth.views import LogoutView, LoginView
 
@@ -25,47 +23,5 @@ urlpatterns = [
             next_page='book:index'
         ),
         name='login'
-    ),
-
-    path(
-        'password_change/',
-        reset.PasswordChangeView.as_view(
-            template_name='users/password_change_form.html'),
-        name='password_change'
-    ),
-
-    path(
-        'password_change/done/',
-        reset.PasswordChangeDoneView.as_view(
-            template_name='users/password_change_done.html'),
-        name='password_change_done'
-    ),
-
-    path(
-        'password_reset/',
-        reset.PasswordResetView.as_view(
-            template_name='users/password_reset_form.html'),
-        name='password_reset'
-    ),
-
-    path(
-        'password_reset/done/',
-        reset.PasswordResetDoneView.as_view(
-            template_name='users/password_reset_done.html'),
-        name='password_reset_done'
-    ),
-
-    path(
-        'reset/<uidb64>/<token>/',
-        reset.PasswordResetConfirmView.as_view(
-            template_name='users/password_reset_confirm.html'),
-        name='password_reset_confirm'
-    ),
-
-    path(
-        'reset/done/',
-        reset.PasswordResetCompleteView.as_view(
-            template_name='users/password_reset_complete.html'),
-        name='password_reset_complete'
     ),
 ]
